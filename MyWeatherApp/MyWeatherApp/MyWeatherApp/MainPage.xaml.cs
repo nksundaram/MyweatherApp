1167e04a4c237a7cddafd7fe769214ab1057fbf3
Kalyanasundaram N <nk_sundaram@outlook.com>
2018-11-16 15:22:35 +0530

    Complete Project

---
+++ MyWeatherApp/MyWeatherApp/MyWeatherApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MyWeatherApp.Models;
+using MyWeatherApp.Repository;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyWeatherApp
+{
+    public partial class MainPage : ContentPage
+    { 
+
+        IWeatherService weatherService;
+    
+        public MainPage()
+        {
+            InitializeComponent();
+            if (weatherService == null)
+                weatherService = new WeatherService();
+        }
+
+        
+
+        public Task<GetCurrentWeatherResult> GetCurrentWeatherAsync(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetForecastWeatherResult> GetFiveDayForecastAsync(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WeatherCondition GetWeatherCondition(WeatherConditionType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void GetWeather_Clicked(object sender, EventArgs e)
+        {
+            var weather = await weatherService.GetCurrentWeatherAsync(cityname.Text);
+           // await DisplayAlert("MyWeatherApp", "looking up weather for: " + cityname.Text, "Ok");
+           if (weather == null)
+            {
+                await DisplayAlert("MyWeatherApp", "Could not get Weather, please", "OK");
+            }
+        }
+    }
+}
